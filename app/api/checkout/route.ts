b4f27ca4ad5923a038958f41ce7b48f7a58023aa
Bhargawanan Bhuyan <bhargawanan@Bhargawanans-MacBook-Pro.local>
2023-11-14 23:47:03 +0530

    commit

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
 
 		const paymentIntent = await stripe.paymentIntents.create({
 			payment_method: paymentMethodId,
-			amount,
+			amount: amount ** 100,
 			currency: 'inr',
 			confirmation_method: 'automatic',
 			confirm: true,
